d8c7ebe5caa03352cae01dc685e8a4831136da12
mdasif8393 <md.asif.8393@gmail.com>
2025-12-14 16:34:28 +0600

    assd

---
+++ module-1-time-and-space-complexity/sum.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int sum = (n * (n + 1)) / 2;
+    // for (int i = 0; i <= n; i++)
+    // {
+    //     sum += i;
+    // }
+
+    cout << sum << endl;
+
+    return 0;
+}
