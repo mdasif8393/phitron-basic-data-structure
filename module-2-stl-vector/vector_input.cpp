ab62c9d09b2c41a335a88848b1bdad2b451f4694
mdasif8393 <md.asif.8393@gmail.com>
2025-12-17 12:21:29 +0600

    2_7 Vector input and output

--- module-2-stl-vector/vector_input.cpp
+++ module-2-stl-vector/vector_input.cpp
@@ -7,31 +7,31 @@ int main()
     int n;
     cin >> n;
 
-    // vector<int> v(n);
+    vector<int> v(n);
 
-    // for (int i = 0; i < n; i++)
-    // {
-    //     cin >> v[i];
-    // }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
 
-    // for (int i = 0; i < n; i++)
-    // {
-    //     cout << v[i] << " ";
-    // }
-    // cout << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
 
     //* input without n
-    vector<int> v1;
-    for (int i = 0; i < n; i++)
-    {
-        int x;
-        cin >> x;
-        v1.push_back(x);
-    }
-    for (int i = 0; i < n; i++)
-    {
-        cout << v1[i] << " ";
-    }
+    // vector<int> v1;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     int x;
+    //     cin >> x;
+    //     v1.push_back(x);
+    // }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << v1[i] << " ";
+    // }
 
     return 0;
 }
