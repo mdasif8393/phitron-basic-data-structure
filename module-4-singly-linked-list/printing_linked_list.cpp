44523b6c916a071adb29f2bb0cfc2f083a8347e5
mdasif8393 <md.asif.8393@gmail.com>
2026-02-16 13:22:53 +0600

    a

---
+++ module-4-singly-linked-list/printing_linked_list.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+
+    head->next = a;
+    a->next = b;
+    b->next = c;
+
+    Node *tmp = head;   // make a temporay pointer and store head pointer to tmp
+    while (tmp != NULL) // when temporary head is null then break the loop
+    {
+        cout << tmp->val << endl;
+        tmp = tmp->next; // make the temporary head to the next head address
+    }
+
+    return 0;
+}
