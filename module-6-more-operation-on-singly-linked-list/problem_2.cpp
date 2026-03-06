23a75b97af512c03ba1eff53b8f5a6614ceaeb55
mdasif8393 <md.asif.8393@gmail.com>
2026-03-06 16:04:09 +0600

    solve problem 2

---
+++ module-6-more-operation-on-singly-linked-list/problem_2.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void add_to_tail(Node *&head, Node *&tail, int val)
+{
+
+    Node *newNode = new Node(val);
+
+    Node *temp = head;
+
+    if (temp == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+    }
+
+    tail->next = newNode;
+    tail = newNode;
+};
+
+void reverse_print(Node *&head)
+{
+    Node *temp = head;
+
+    if (temp == NULL)
+    {
+        return;
+    }
+
+    reverse_print(temp->next);
+    cout << temp->val << " ";
+};
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+
+    while (true)
+    {
+        cin >> val;
+
+        if (val == -1)
+        {
+            break;
+        }
+
+        add_to_tail(head, tail, val);
+    }
+
+    reverse_print(head);
+
+    return 0;
+}
