f709f9b7615c724fbb991feefbea00148a3601b0
mdasif8393 <md.asif.8393@gmail.com>
2026-03-11 19:05:27 +0600

    8_1 Doubly linked list

---
+++ module-8-doubly-linked-list/doubly_linked_list.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *tail = new Node(30);
+
+    head->next = a;
+
+    a->prev = head; // connect with previous node
+    a->next = tail; // connect with next node
+
+    tail->prev = a;
+
+    return 0;
+}
