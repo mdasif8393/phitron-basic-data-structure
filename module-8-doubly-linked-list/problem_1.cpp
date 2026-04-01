01e5e647c59c575b14c602dd66892def6746f96e
mdasif8393 <md.asif.8393@gmail.com>
2026-04-01 12:33:44 +0600

    s

---
+++ module-8-doubly-linked-list/problem_1.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *prev;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+
+    Node *new_node = new Node(val);
+
+    if (head == NULL)
+    {
+        head = new_node;
+        tail = new_node;
+        return;
+    };
+
+    tail->next = new_node;
+    new_node->prev = tail;
+    tail = new_node;
+};
+
+void compare_linked_lists(Node *head_1, Node *head_2)
+{
+
+    Node *temp_1 = head_1;
+    Node *temp_2 = head_2;
+
+    int count_1 = 0;
+    int count_2 = 0;
+
+    while (temp_1 != NULL)
+    {
+        count_1++;
+        temp_1 = temp_1->next;
+    }
+    while (temp_2 != NULL)
+    {
+        count_2++;
+        temp_2 = temp_2->next;
+    }
+
+    if (temp_1 != temp_2)
+    {
+        cout << "NO";
+        return;
+    }
+    else
+    {
+    }
+};
+
+void display_linked_list(Node *head)
+{
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+};
+
+int main()
+{
+    Node *head_1 = NULL;
+    Node *tail_1 = NULL;
+
+    int val_1;
+
+    while (true)
+    {
+        cin >> val_1;
+        if (val_1 == -1)
+        {
+
+            break;
+        }
+        insert_at_tail(head_1, tail_1, val_1);
+    }
+
+    Node *head_2 = NULL;
+    Node *tail_2 = NULL;
+
+    int val_2;
+
+    while (true)
+    {
+        cin >> val_2;
+        if (val_2 == -1)
+        {
+
+            break;
+        }
+        insert_at_tail(head_2, tail_2, val_2);
+    }
+
+    compare_linked_lists(head_1, head_2);
+
+    return 0;
+}
---
+++ module-8-doubly-linked-list/readme.md
@@ -0,0 +1,1 @@
+## Practice Problems: https://docs.google.com/document/d/1FaTqupuytyu0Ktlz9fdXU41T1EDVqWjNb3xwraCWgxo/edit?tab=t.0
