aae67950d90c4a43bef356b2f7b1654085efff2c
mdasif8393 <md.asif.8393@gmail.com>
2026-04-22 21:53:43 +0600

    get max height

---
+++ module-16-operation-on-binary-tree/get_max_height.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *input_tree()
+{
+    int val;
+    cin >> val;
+
+    // make root node
+    Node *root;
+    (val == -1) ? root = NULL : root = new Node(val);
+
+    // make a queue and push root node here
+    queue<Node *> q;
+    if (root)
+        q.push(root);
+
+    while (!q.empty())
+    {
+        //* Step 1. Ber kore ana queue theke
+        Node *p = q.front();
+        q.pop();
+
+        //* Step 2:
+        // Take 2 input  from input.txt file for queue top value
+        int l, r;
+        cin >> l >> r;
+
+        // Make left and right nodes
+        Node *myLeft, *myRight;
+        (l == -1) ? myLeft = NULL : myLeft = new Node(l);
+        (r == -1) ? myRight = NULL : myRight = new Node(r);
+
+        // connect left and right nodes
+        p->left = myLeft;
+        p->right = myRight;
+
+        //* Step 3: Children push kora queue te
+        if (p->left)
+            q.push(p->left);
+        if (p->right)
+            q.push(p->right);
+    };
+    return root;
+};
+
+int max_height(Node *root)
+{
+    // if root null then return 0. For empty tree
+    if (root == NULL)
+        return 0;
+    // For leaf node return 0
+    if (root->left == NULL && root->right == NULL)
+        return 0;
+
+    // calculate left and right nodes
+    int l = max_height(root->left);
+    int r = max_height(root->right);
+
+    // return max height fromleft and right node + 1
+    return max(l, r) + 1;
+};
+
+int main()
+{
+    Node *root = input_tree();
+    cout << max_height(root);
+
+    return 0;
+}
